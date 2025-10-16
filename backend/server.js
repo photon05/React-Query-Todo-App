1f7d866c8d3c149c747f6275bb1bfdca12d13db6
photon05 <arjunbhardwaj.ab.2003@gmail.com>
2025-10-16 18:34:26 +0530

    mongoose connection changed

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,9 @@ app.use(express.json());
 
 // MongoDB Connection
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1);
-  }
-};
-connectDB();
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+})
 
 app.get('/', (req, res) => {
   res.send("This is the todo app");
