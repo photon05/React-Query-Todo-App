03b64f4107ee1fd5590b1d22c65a38d22bbb68a3
photon05 <arjunbhardwaj.ab.2003@gmail.com>
2025-10-24 17:52:57 +0530

    Revert "mongoose connection changed"
    This reverts commit 1f7d866c8d3c149c747f6275bb1bfdca12d13db6.

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,16 @@ app.use(express.json());
 
 // MongoDB Connection
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-})
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 app.get('/', (req, res) => {
   res.send("This is the todo app");
