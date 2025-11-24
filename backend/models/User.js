8587c1dc55cf4d99b61a7ad165243f24d6ba5016
photon05 <arjunbhardwaj.ab.2003@gmail.com>
2025-11-24 13:27:34 +0530

    Adding User Modal in database

---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    userId: Number,
+    username: String,
+    password: String
+})
+
+export default mongoose.model("User", UserSchema)
